451ebce64208137b98ec493158b93c6086c38b0c
florinhegedus <florinxhgds@gmail.com>
2020-07-08 16:02:52 +0300

    Second Commit

--- wwii.py
+++ wwii.py
@@ -8,7 +8,7 @@ dis_width=1300
 dis_height=700
 dis=pygame.display.set_mode((dis_width,dis_height))
 pygame.display.update()
-pygame.display.set_caption("WWII Invasion of London")
+pygame.display.set_caption("WWII The Invasion of London")
 
 #Clock
 clock = pygame.time.Clock()
@@ -24,11 +24,13 @@ green = (0,255,0)
 
 #Fonts
 font1 = pygame.font.Font('ChursaechsischeFraktur.ttf', 30)
+font2 = pygame.font.Font('Qiko.ttf', 30)
+font3 = pygame.font.Font('Marsh Stencil Regular.otf', 60)
 
 #Title text function
 def title():
-	text = font1.render("Nazi Germany", True, black)
-	dis.blit(text, [500, 200])
+	text = font3.render("WWII THE INVASION OF LONDON", True, black)
+	dis.blit(text, [200, 300])
 
 #Images
 def swastika():
@@ -43,8 +45,12 @@ def floor():
 
 #Score
 def display_score(score):
-	text = font1.render("Score: " + str(score), True, black)
-	dis.blit(text, [1100, 20])
+	text = font2.render(str(score), True, black)
+	dis.blit(text, [1250, 20])
+
+def display_final_score(score):
+	text = font3.render("Score: " + str(score), True, black)
+	dis.blit(text, [500, 420])
 
 #Tank
 class Tank:
@@ -57,7 +63,7 @@ class Tank:
 		self.image = pygame.transform.scale(self.image, [200, 80])
 		dis.blit(self.image, [self.x, self.y])
 	def move(self):
-		self.x -= self.velocity * dt
+		self.x -= self.velocity
 	def get_mask(self):
 		return pygame.mask.from_surface(self.image)
 	def collide(self, bomb):
@@ -84,6 +90,8 @@ class Plane:
 		self.x -= self.velocity
 		if self.x > 1300:
 			self.x = -200
+			self.velocity = -2
+			self.bomb.velocity_x = -2
 			self.bomb_launched = False
 			self.bomb.x = -120
 			self.bomb.y = 150
@@ -92,6 +100,10 @@ class Plane:
 			self.bomb.move()
 		else:
 			self.bomb.drop()
+	def accelerate(self):
+		self.velocity = -4
+		if not self.bomb_launched:
+			self.bomb.velocity_x = -4
 
 #Bomb
 class Bomb:
@@ -121,12 +133,50 @@ class Bomb:
 	def get_mask(self):
 		return pygame.mask.from_surface(self.image)
 
+def gameIntro():
+	intro = True
+	while intro:
+		dis.fill(red)
+		title()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT :
+				pygame.quit()
+				quit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					intro = False
+				if event.key == pygame.K_q:
+					pygame.quit()
+					quit()
+		pygame.display.update()
+
+def gameOver(score):
+	pause = True
+	while pause:
+		dis.fill(red)
+		title()
+		display_final_score(score)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT :
+				pygame.quit()
+				quit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					pause = False
+					gameLoop()
+				if event.key == pygame.K_q:
+					pygame.quit()
+					quit()
+		pygame.display.update()
+
+
+
 def gameLoop():
 	game_over = False
 	score = 0
 	bomb = Bomb(-120)
 	plane = Plane(-200, bomb)
-	tanks = [Tank(1300), Tank(1960)]
+	tanks = [Tank(1300), Tank(1960), Tank(2600)]
 	while not game_over:
 		dis.fill(red)
 		for event in pygame.event.get():
@@ -137,6 +187,8 @@ def gameLoop():
 					game_over = True
 				if event.key == pygame.K_SPACE:
 					plane.bomb_launched = True
+				if event.key == pygame.K_w:
+					plane.accelerate()
 
 		floor()
 		display_score(score)
@@ -145,6 +197,7 @@ def gameLoop():
 		for tank in tanks:
 			if tank.x < -200:
 				tank.x = 1300
+				game_over = True
 			tank.move()
 			tank.draw()
 			if tank.collide(bomb):
@@ -152,7 +205,11 @@ def gameLoop():
 				tanks.remove(tank)
 				new_tank = Tank(random.randint(1500, 2500))
 				tanks.append(new_tank)
-		clock.tick(60)
+				if score%25 == 0:
+					new_tank = Tank(random.randint(1500, 2500))
+					tanks.append(new_tank)
 		pygame.display.update()
+	gameOver(score)
 
+gameIntro()
 gameLoop()
